cd59a898f885b094e444045c3c88d6831aef391a
kartikeymishra2202 <mharmilap@gmail.com>
2024-06-16 17:18:52 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 8, packed: false },
+//   { id: 4, description: "Atm", quantity: 3, packed: false },
+// ];
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleClearItems() {
+    const confirmed = window.confirm(
+      "are you sure you want to delete all items"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+        onClearItem={handleClearItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
